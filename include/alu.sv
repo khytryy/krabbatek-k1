4e022e4ccc1518fc1044b1939161b48b8588e81e
Yehor Khytryy <yehor.khytryy@gmail.com>
2026-03-15 14:26:17 +0000

    Add alu chip

--- include/alu.sv
+++ include/alu.sv
@@ -1,14 +1,57 @@
+`include "adders.sv"
+
 module alu
 (
-    input logic         clk, 
     input logic[31:0]   a,
     input logic[31:0]   b,
-    input logic[1:0]    op,
-    output logic[31:0]  res
+    
+    input logic         za,
+    input logic         na,
+    input logic         zb,
+    input logic         nb,
+    input logic[2:0]    op,
+    input logic         no,
+
+    output logic        lt,
+    output logic        gt,
+    output logic        zr,
+    output logic[31:0]  out
 );
 
-    always @(posedge clk) begin;
-        
+    logic[31:0] temp_a, temp_b, raw_out, add_result;
+
+    adder32bit a0(
+        .a   (temp_a),
+        .b   (temp_b),
+        .cin (1'b0),
+        .res (add_result)
+    );
+
+    always_comb begin
+        // Zero / negate A
+        temp_a = za ? 32'b0 : a;
+        if (na) temp_a = ~temp_a;
+
+        // Zero / negate B
+        temp_b = zb ? 32'b0 : b;
+        if (nb) temp_b = ~temp_b;
+
+        // Opcodes
+        case (op)
+            3'b001: raw_out = add_result;
+            3'b010: raw_out = temp_a & temp_b;
+            3'b011: raw_out = temp_a | temp_b;
+            3'b100: raw_out = temp_a ^ temp_b;
+            3'b101: raw_out = ~temp_a;
+            3'b110: raw_out = ~temp_b;
+            default: raw_out = 32'b0;
+        endcase
+
+        out = no ? ~raw_out : raw_out;
     end
+
+    assign zr = (out == 32'b0);
+    assign lt = out[31];
+    assign gt = !zr && !lt;
 
 endmodule
